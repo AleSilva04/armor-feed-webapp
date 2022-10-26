3de9562b640f153cba8df4bb2f30c15c6dd1e553
Oscar Heredia <u201913874@upc.edu.pe>
2022-10-26 00:31:25 -0500

    notification: add notification services

--- src/notification/services/payments.service.js
+++ src/notification/services/payments.service.js
@@ -1,11 +1,11 @@
 import http from "../../shared/services/http-common.js";
 
-class AddressesService {
-    endPoint = "/addresses";
+class PaymentsService {
+    endPoint = "/payments";
 
     create(createAddressDto) {
         return http.post(this.endPoint, createAddressDto);
     }
 }
 
-export default new AddressesService();
+export default new PaymentsService();
